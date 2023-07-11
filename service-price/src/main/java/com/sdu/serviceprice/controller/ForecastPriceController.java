a8c1304f233cb93f5f96552ce1e0d81cd5567dcb
lihaopeng <752869332@qq.com>
2023-07-12 01:09:25 +0800

    service-price 骨架编写完成

---
+++ service-price/src/main/java/com/sdu/serviceprice/controller/ForecastPriceController.java
@@ -0,0 +1,35 @@
+package com.sdu.serviceprice.controller;
+
+import com.sdu.internalcommon.dto.ResponseResult;
+import com.sdu.internalcommon.request.ForecastPriceDTO;
+import com.sdu.serviceprice.service.ForecastPriceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author LHP
+ * @date 2023-07-12 0:44
+ * @description
+ */
+
+@RestController
+public class ForecastPriceController {
+
+    @Autowired
+    private ForecastPriceService forecastPriceService;
+
+    @PostMapping("/forecast-price")
+    public ResponseResult forecastPrice(@RequestBody ForecastPriceDTO forecastPriceDTO) {
+
+        String depLongitude = forecastPriceDTO.getDepLongitude();
+        String depLatitude = forecastPriceDTO.getDepLatitude();
+        String destLongitude = forecastPriceDTO.getDestLongitude();
+        String destLatitude = forecastPriceDTO.getDestLatitude();
+
+        return forecastPriceService.forecastPrice(depLongitude, depLatitude, destLongitude, destLatitude);
+
+    }
+
+}
---
+++ service-price/src/main/java/com/sdu/serviceprice/service/ForecastPriceService.java
@@ -0,0 +1,44 @@
+package com.sdu.serviceprice.service;
+
+import com.sdu.internalcommon.dto.ResponseResult;
+import com.sdu.internalcommon.response.ForecastPriceResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author LHP
+ * @date 2023-07-12 0:46
+ * @description
+ */
+
+@Service
+@Slf4j
+public class ForecastPriceService {
+
+    /**
+     * 根据出发地和目的地经纬度，预估价格
+     * @param depLongitude
+     * @param depLatitude
+     * @param destLongitude
+     * @param destLatitude
+     * @return
+     */
+    public ResponseResult forecastPrice(String depLongitude, String depLatitude, String destLongitude, String destLatitude) {
+
+        log.info("出发地经度：" + depLongitude);
+        log.info("出发地纬度：" + depLatitude);
+        log.info("目的地经度：" + destLongitude);
+        log.info("目的地纬度：" + destLatitude);
+
+        log.info("调用地图服务，查询距离和时长");
+
+        log.info("读取计价规则");
+
+        log.info("根据距离、时长和计价规则，计算价格");
+
+        ForecastPriceResponse forecastPriceResponse = new ForecastPriceResponse();
+        forecastPriceResponse.setPrice(12.34);
+        return ResponseResult.success(forecastPriceResponse);
+    }
+
+}

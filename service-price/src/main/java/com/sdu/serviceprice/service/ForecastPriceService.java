d14130adc1cd98e6be2fc4c41ef7bbb7afebe93d
lihaopeng <752869332@qq.com>
2023-07-12 09:32:33 +0800

    service-price 调用 service-map

--- service-price/src/main/java/com/sdu/serviceprice/service/ForecastPriceService.java
+++ service-price/src/main/java/com/sdu/serviceprice/service/ForecastPriceService.java
@@ -1,8 +1,12 @@
 package com.sdu.serviceprice.service;
 
 import com.sdu.internalcommon.dto.ResponseResult;
+import com.sdu.internalcommon.request.ForecastPriceDTO;
+import com.sdu.internalcommon.response.DirectionResponse;
 import com.sdu.internalcommon.response.ForecastPriceResponse;
+import com.sdu.serviceprice.remote.ServiceMapClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,6 +18,9 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class ForecastPriceService {
+
+    @Autowired
+    private ServiceMapClient serviceMapClient;
 
     /**
      * 根据出发地和目的地经纬度，预估价格
@@ -29,6 +36,16 @@ public class ForecastPriceService {
         log.info("出发地纬度：" + depLatitude);
         log.info("目的地经度：" + destLongitude);
         log.info("目的地纬度：" + destLatitude);
+
+        ForecastPriceDTO forecastPriceDTO = new ForecastPriceDTO();
+        forecastPriceDTO.setDepLongitude(depLongitude);
+        forecastPriceDTO.setDepLatitude(depLatitude);
+        forecastPriceDTO.setDestLongitude(destLongitude);
+        forecastPriceDTO.setDestLatitude(destLatitude);
+        ResponseResult<DirectionResponse> direction = serviceMapClient.direction(forecastPriceDTO);
+        Integer distance = direction.getData().getDistance();
+        Integer duration = direction.getData().getDuration();
+        log.info("距离：" + distance + "，时长：" + duration);
 
         log.info("调用地图服务，查询距离和时长");
 

6dd7599a7f8bd30920dd0726359dc55b62fa44bb
lihaopeng <752869332@qq.com>
2023-07-14 00:40:35 +0800

    api-driver 上传位置功能，获取 tid 和 trid 完成

--- service-driver-user/src/main/java/com/sdu/servicedriveruser/controller/CarController.java
+++ service-driver-user/src/main/java/com/sdu/servicedriveruser/controller/CarController.java
@@ -5,6 +5,7 @@ import com.sdu.internalcommon.dto.Car;
 import com.sdu.internalcommon.dto.ResponseResult;
 import com.sdu.servicedriveruser.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -24,6 +25,11 @@ public class CarController {
     @PostMapping("/car")
     public ResponseResult addCar(@RequestBody Car car){
         return carService.addCar(car);
+    }
+
+    @GetMapping("/car")
+    public ResponseResult<Car> getCarById(Long carId){
+        return carService.getCarById(carId);
     }
 
 }

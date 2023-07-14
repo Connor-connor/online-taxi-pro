6d066c3580b5724281040d6dbc4ccce58b14946c
lihaopeng <752869332@qq.com>
2023-07-14 10:15:48 +0800

    自动生成 order_info 表的代码

---
+++ service-order/src/main/java/com/sdu/serviceorder/ServiceOrderApplication.java
@@ -0,0 +1,18 @@
+package com.sdu.serviceorder;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@SpringBootApplication
+@EnableFeignClients
+@MapperScan("com.sdu.serviceorder.mapper")
+public class ServiceOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceOrderApplication.class);
+    }
+
+}
---
+++ service-order/src/main/java/com/sdu/serviceorder/mapper/OrderInfoMapper.java
@@ -0,0 +1,16 @@
+package com.sdu.serviceorder.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sdu.internalcommon.dto.OrderInfo;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author LHP
+ * @date 2023-07-14 10:10
+ * @description
+ */
+
+@Repository
+public interface OrderInfoMapper extends BaseMapper<OrderInfo> {
+
+}
